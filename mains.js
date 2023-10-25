879b8b99564cf188526e88a29b2c5f9cf7c6cc6a
não tem nome <134523868+anjosemperna@users.noreply.github.com>
2023-10-25 11:49:31 -0300

    xac

--- mains.js
+++ mains.js
@@ -4,3 +4,35 @@ function tocaSomPom(){
 }    
 
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+function tocaSomClap(){
+    document.querySelector('.tecla_clap').onclick = tocaSomClap;
+}
+
+function tocaSomTim(){
+    document.querySelector('.tecla_clap').onclick = tocaSomTim;
+}    
+
+function tocaSomPuff(){
+    document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+} 
+
+function tocaSomSplash(){
+    document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+} 
+
+function tocaSomToim(){
+    document.querySelector('.tecla_Toim').onclick = tocaSomToim;
+} 
+
+function tocaSomPsh(){
+    document.querySelector('.tecla_psh').onclick = tocaSomPsh;
+} 
+
+function tocaSomTic(){
+    document.querySelector('.tecla_tic').onclick = tocaSomTic;
+} 
+
+function tocaSomTom(){
+    document.querySelector('.tecla_tom').onclick = tocaSomTom;
+} 
